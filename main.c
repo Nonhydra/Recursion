7ac64062dd82ac9a8b36dcd613b77ebee54e97cd
Kirill <desving12345@gmail.com>
2017-06-25 15:09:17 +0700

    Fixed main.c

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-int sum_array_rec(int arr[], int num)
+int sum_array_rec(int arr[], int num, int sum)
 {
 	if (num == -1) {
 		return 0;
 	}
-	return (arr[num - 1] + sum_array_rec(arr, num - 1));
+
+	sum += arr[num - 1] + sum_array_rec(arr, num - 1, sum);
+	return sum;
 }
 
 int sum_array_iter(int arr[], int num, int sum)
@@ -56,14 +58,15 @@ void print_array(int arr[], int num)
 int main()
 {
 	int num = 5;
+	int sum = 0;
 	int arr[5] = {1, 2, 3, 4, 5};
 	print_array(arr, num);
 	
 	//Recursive
-	printf("\nsum_array_recursive = %d\n\n", sum_array_rec(arr, num));
-	
+	printf("\nsum_array_recursive = %d\n\n", sum_array_rec(arr, num, sum));
+
+	sum = 0;
 	//Iteration
-	int sum = 0;
 	printf("\nsum_array_iteration = %d\n\n", sum_array_iter(arr, num, sum));
 	int n[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	print_array(n, 11);
